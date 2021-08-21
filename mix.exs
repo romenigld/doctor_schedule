cfb290c4c2de40fe75bc75360897621166d19a73
Romenig Lima Damasio <romenigld@gmail.com>
2021-08-21 14:51:10 +0200

    CodeCoveralls Report CI ( add coveralls.json on mix)

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule DoctorSchedule.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
         "coveralls.json": :test
       ]
     ]
