70b33dd7089ec10b72803190fc86e23431868aac
Romenig Lima Damasio <romenigld@gmail.com>
2021-08-27 11:20:50 +0200

    Sessions Test

--- lib/doctor_schedule_web/controllers/auth/guardian.ex
+++ lib/doctor_schedule_web/controllers/auth/guardian.ex
@@ -1,8 +1,8 @@
 defmodule DoctorScheduleWeb.Auth.Guardian do
   use Guardian, otp_app: :doctor_schedule
 
-  alias DoctorSchedule.Accounts.Services.Session
   alias DoctorSchedule.Accounts.Repositories.AccountRepository
+  alias DoctorSchedule.Accounts.Services.Session
 
   def subject_for_token(user, _claims), do: {:ok, to_string(user.id)}
 
