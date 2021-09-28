2a5d9433c0834a075050d7d8d093011ce2606239
Romenig Lima Damasio <romenigld@gmail.com>
2021-09-28 18:02:04 +0200

    Create Appointment Tests

--- test/doctor_schedule/appointments/repositories/provider_repository_test.exs
+++ test/doctor_schedule/appointments/repositories/provider_repository_test.exs
@@ -7,30 +7,27 @@ defmodule DoctorSchedule.Appointments.Repositories.ProviderRepositoryTest do
 
   describe "Tests Provider in Appointments" do
     test "all_month_from_provider/3 returns all appointments from provider in a month" do
-      appointment =
-        AppointmentFixture.appointment_fixture()
+      appointment = AppointmentFixture.appointment_fixture()
+
       appointment =
         appointment
         |> Repo.preload(:provider)
 
-      year =
-        appointment.date.year
-      month =
-        appointment.date.month
+      year = appointment.date.year
+      month = appointment.date.month
 
       assert ProviderRepository.all_month_from_provider(appointment.provider.id, year, month)
              |> Enum.count() == 1
     end
 
     test "all_day_from_provider/2 returns all appointments from provider in a month" do
-      appointment =
-        AppointmentFixture.appointment_fixture()
+      appointment = AppointmentFixture.appointment_fixture()
+
       appointment =
         appointment
         |> Repo.preload(:provider)
 
-      date =
-        NaiveDateTime.to_date(appointment.date)
+      date = NaiveDateTime.to_date(appointment.date)
 
       assert ProviderRepository.all_day_from_provider(appointment.provider.id, date)
              |> Enum.count() == 1
